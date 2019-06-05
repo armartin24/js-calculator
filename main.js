e0af2cc15ddc698b8c6c18a039c71c8e0cc08551
armartin24 <50032006+armartin24@users.noreply.github.com>
2019-06-05 20:49:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,64 @@ function number_one() {
 function number_two() {
   $("#place").append("2");
 }
+
+function number_three() {
+  $("#place").append("3");
+}
+
+
+function number_four() {
+  $("#place").append("4");
+}
+
+function number_five() {
+  $("#place").append("5");
+}
+
+
+function number_six() {
+  $("#place").append("6");
+}
+
+function number_seven() {
+  $("#place").append("7");
+}
+
+
+function number_eight() {
+  $("#place").append("8");
+}
+
+
+function number_nine() {
+  $("#place").append("9");
+}
+
+
+function number_zero() {
+  $("#place").append("0");
+}
+
+function number_plus() {
+  $("#place").append("+");
+}
+
+
+function number_minus() {
+  $("#place").append("-");
+}
+
+function number_divide() {
+  $("#place").append("/");
+}
+
+
+function number_times() {
+  $("#place").append("x");
+}
+
+function number_equals() {
+  $("#place").append("equals");
+}
+
+
